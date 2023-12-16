b7ff56e31c04b3b1d6efa8183ab8a5920314a2bb
Giovanni Berti <dev.giovanniberti@gmail.com>
2023-12-16 12:57:36 +0100

    refactor(exception): tweak `InvalidBoxTypeException` message

--- src/main/java/io/github/giovanniberti/base_media_file_parser/boxes/InvalidBoxTypeException.java
+++ src/main/java/io/github/giovanniberti/base_media_file_parser/boxes/InvalidBoxTypeException.java
@@ -1,18 +1,18 @@
 package io.github.giovanniberti.base_media_file_parser.boxes;
 
-import java.util.Arrays;
-
 public class InvalidBoxTypeException extends RuntimeException {
 
     public InvalidBoxTypeException(byte[] rawValue) {
-        super("Expected box type, got %s (%s)".formatted(bytesAsHex(rawValue), new String(rawValue)));
+        super("Expected known box type, got [%s] (%s)".formatted(bytesAsHex(rawValue), new String(rawValue)));
     }
 
     private static String bytesAsHex(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
         for (byte b : bytes) {
-            sb.append(String.format("%02X ", b));
+            sb.append(String.format("%02x ", b));
         }
+
+        sb.deleteCharAt(sb.length() - 1);
 
         return sb.toString();
     }
