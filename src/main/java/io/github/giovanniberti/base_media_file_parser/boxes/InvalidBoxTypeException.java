1bcead5f85decdab8222146683657cd150311b5b
Giovanni Berti <dev.giovanniberti@gmail.com>
2023-12-16 12:52:23 +0100

    feat(exception): add better diagnostics to `InvalidBoxException`

--- src/main/java/io/github/giovanniberti/base_media_file_parser/boxes/InvalidBoxTypeException.java
+++ src/main/java/io/github/giovanniberti/base_media_file_parser/boxes/InvalidBoxTypeException.java
@@ -1,8 +1,19 @@
 package io.github.giovanniberti.base_media_file_parser.boxes;
 
+import java.util.Arrays;
+
 public class InvalidBoxTypeException extends RuntimeException {
 
     public InvalidBoxTypeException(byte[] rawValue) {
-        super("Expected box type, got %s".formatted(new String(rawValue)));
+        super("Expected box type, got %s (%s)".formatted(bytesAsHex(rawValue), new String(rawValue)));
+    }
+
+    private static String bytesAsHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02X ", b));
+        }
+
+        return sb.toString();
     }
 }
