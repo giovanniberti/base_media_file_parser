4006afc7fbf188ce25fd59eccd54f3bc2c440767
Giovanni Berti <dev.giovanniberti@gmail.com>
2023-12-16 21:22:11 +0100

    test: added check of snapshot against fully parsed structure

--- src/test/java/io/github/giovanniberti/base_media_file_parser/parser/ParserTest.java
+++ src/test/java/io/github/giovanniberti/base_media_file_parser/parser/ParserTest.java
@@ -1,10 +1,13 @@
 package io.github.giovanniberti.base_media_file_parser.parser;
 
+import io.github.giovanniberti.base_media_file_parser.boxes.BoxType;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest {
     @Test
@@ -12,9 +15,50 @@ public class ParserTest {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         InputStream is = classloader.getResourceAsStream("text0.mp4");
 
+        List<BoxNode> expectedStructure = List.of(
+                new BoxNode(
+                        BoxType.MOOF,
+                        181,
+                        List.of(
+                                new BoxNode(
+                                        BoxType.MFHD,
+                                        16
+                                ),
+                                new BoxNode(
+                                        BoxType.TRAF,
+                                        157,
+                                        List.of(
+                                                new BoxNode(
+                                                        BoxType.TFHD,
+                                                        24
+                                                ),
+                                                new BoxNode(
+                                                        BoxType.TRUN,
+                                                        20
+                                                ),
+                                                new BoxNode(
+                                                        BoxType.UUID,
+                                                        44
+                                                ),
+                                                new BoxNode(
+                                                        BoxType.UUID,
+                                                        61
+                                                )
+                                        )
+                                )
+                        )
+                ),
+                new BoxNode(
+                        BoxType.MDAT,
+                        17908
+                )
+        );
+
         try (Parser parser = new Parser(is)) {
-            List<BoxNode> boxNode = parser.parse();
-            System.out.println(boxNode);
+            List<BoxNode> parsedTree = parser.parse();
+            System.out.println(parsedTree);
+
+            assertEquals(parsedTree, expectedStructure);
         } catch (ParsingFailedException e) {
             System.out.println(e.parsedTree);
             throw e;
