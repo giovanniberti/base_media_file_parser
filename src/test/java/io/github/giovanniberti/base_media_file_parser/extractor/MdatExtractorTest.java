eae711b5c5e55894d53eebbdb9ddbcdd6ebd1379
Giovanni Berti <dev.giovanniberti@gmail.com>
2023-12-17 16:13:46 +0100

    test(extractor): add hash check in snapshot test

--- src/test/java/io/github/giovanniberti/base_media_file_parser/extractor/MdatExtractorTest.java
+++ src/test/java/io/github/giovanniberti/base_media_file_parser/extractor/MdatExtractorTest.java
@@ -7,15 +7,18 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static io.github.giovanniberti.base_media_file_parser.ByteUtils.bytesAsHex;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MdatExtractorTest {
     @Test
-    void canExtractImagesFromSnapshot() throws IOException, DocumentException {
+    void canExtractImagesFromSnapshot() throws IOException, DocumentException, NoSuchAlgorithmException {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         InputStream is = classloader.getResourceAsStream("text0.mp4");
 
@@ -27,8 +30,20 @@ public class MdatExtractorTest {
 
             assertEquals(expectedIds, images.keySet());
 
-            // Here we could use a hash function like md5 to check consistency of the extracted images
-            // Left as an exercise for the reader :)
+            Map<String, String> imageHashes = Map.of(
+                    "image001", "485eec5ba75f0f86e391722914ae367250071014f3d5cdcefd78ff972ffe74bc",
+                    "image002", "79084c7f2a7c4c62ab0d45b38fd38f356ede6dafd1c90c1189c7eb11c80529af",
+                    "image003", "79084c7f2a7c4c62ab0d45b38fd38f356ede6dafd1c90c1189c7eb11c80529af"
+            );
+
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            for (Map.Entry<String, byte[]> image : images.entrySet()) {
+                digest.reset();
+                String sha256sum = bytesAsHex(digest.digest(image.getValue())).replace(" ", "");
+
+                assertEquals(imageHashes.get(image.getKey()), sha256sum);
+            }
         }
     }
 }
